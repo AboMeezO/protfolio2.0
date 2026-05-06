b324523fd1cd17e6a1fc1a4e7137a2789bedf49f
AboMeezO <abomeezo2@gmail.com>
2026-05-06 11:10:50 +0300

    assets: export new icons and images for technologies and projects

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,6 +8,7 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
+import docker from "./tech/docker.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -16,6 +17,7 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
 import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
 
 import AboMeezO from "./AboMeezO.jpg";
 import MeezOStudios from "./MeezoStudios.gif";
@@ -48,6 +50,7 @@ export {
   menu,
   close,
   css,
+  docker,
   git,
   html,
   javascript,
@@ -56,5 +59,6 @@ export {
   reactjs,
   tailwind,
   threejs,
+  typescript,
   aref,
 };
